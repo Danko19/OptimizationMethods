7b7b5553d7dd3f3bb2d88ddeb1355549090e7763
Danko <wally19@bk.ru>
2020-02-24 14:49:03 +0400

    FibonacciMethod fixup

--- FibonacciMethod.py
+++ FibonacciMethod.py
@@ -6,19 +6,20 @@ def FindMinimum(a: float, b: float, e: float, func: Callable[[float], float]) ->
     print("Called FindMinimum by FibonacciMethod with params a={}, b={}, e={}".format(a, b, e))
     d = b - a
     fibonacci = TakeThreeFibonacciNumbersWhileLessThan(d / e)
-    x = a + fibonacci[0] / fibonacci[2] * d
-    y = a + fibonacci[1] / fibonacci[2] * d
     update_fx = True
     update_fy = True
     iterations = 0
     measurements = 0
+    k = 0
     while True:
         iterations += 1
         if update_fx:
+            x = a + (b - a) * fibonacci[-3 - k] / fibonacci[-1-k]
             fx = func(x)
             update_fx = False
             measurements += 1
         if update_fy:
+            y = a + (b - a) * fibonacci[-2 - k] / fibonacci[-1-k]
             fy = func(y)
             update_fy = False
             measurements += 1
@@ -28,7 +29,6 @@ def FindMinimum(a: float, b: float, e: float, func: Callable[[float], float]) ->
             a = x
             x = y
             fx = fy
-            y = a + b - x
             update_fy = True
         else:
             fmin = fx
@@ -36,16 +36,14 @@ def FindMinimum(a: float, b: float, e: float, func: Callable[[float], float]) ->
             b = y
             y = x
             fy = fx
-            x = a + b - y
             update_fx = True
+        k += 1
         if b - a < e:
             print("Iterations count={}, measurements count={}".format(iterations, measurements))
             return (xmin, fmin)
 
 def TakeThreeFibonacciNumbersWhileLessThan(l: float):
     fubonacci = [1, 1, 2]
-    while fubonacci[2] < l:
-        fubonacci[0] = fubonacci[1]
-        fubonacci[1] = fubonacci[2]
-        fubonacci[2] = fubonacci[0] + fubonacci[1]
+    while fubonacci[-1] < l:
+        fubonacci.append(fubonacci[-1] + fubonacci[-2])
     return fubonacci
