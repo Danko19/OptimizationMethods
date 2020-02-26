8a42a6fd22801279cef8a3abdcd5c391c4eb1e53
Danko <wally19@bk.ru>
2020-02-26 23:26:44 +0400

    NewtonRaphsonMethod

--- Lab2.py
+++ Lab2.py
@@ -1,6 +1,7 @@
 import DirectcoordinateDescentMethod
 import HookeJeevesMethod
 import GradientDescentMethod
+import NewtonRaphsonMethod
 
 
 def function(x):
@@ -11,6 +12,10 @@ def gradient(x):
     print("x={}, y={}".format(x[0], x[1]))
     return [-2 * x[0] + 3, -14 * x[1] + 1]
 
+def reversedMatrix(x):
+    print("x={}, y={}".format(x[0], x[1]))
+    return [[-1/2, 0], [0, -1/14]]
+
 func = lambda x: -(x[0] ** 2) - 7 * x[1] ** 2 + 3 * x[0] + x[1]
 result = DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.01, 0.01)
 print(result)
@@ -19,4 +24,7 @@ result = HookeJeevesMethod.FindMinimum(function, [0, 0], 0.1, 0.01)
 print(result)
 
 result = GradientDescentMethod.FindMinimum(function, gradient, [0, 0], 0.01)
+print(result)
+
+result = NewtonRaphsonMethod.FindMinimum(function, gradient, reversedMatrix, [0, 0], 0.01)
 print(result)
---
+++ NewtonRaphsonMethod.py
@@ -0,0 +1,62 @@
+from typing import List
+from typing import Tuple
+from typing import Callable
+import GoldenRatioMathod
+import SvennMethod
+import math
+
+def FindMinimum(func: Callable[[List[float]], float], gradient: Callable[[List[float]], List[float]], reverseMatrix: Callable[[List[float]], List[List[float]]], startPoint: List[float], e: float) -> List[Tuple[List[float], float]]:
+    trace = [(startPoint.copy(), func(startPoint))]
+    while True:
+        (previousPoint, _) = trace[-1]
+        grad = gradient(previousPoint)
+        if GetLength(grad) <= e:
+            return trace
+        matrix = reverseMatrix(previousPoint)
+        direction = GetMatrixMultiply(matrix, grad)
+        singleVarFunc = GetSignleVariableFunc(func, previousPoint, direction)
+        (a, b) = SvennMethod.FindUnimodalSegment(0, 0.01, singleVarFunc)
+        (l, newValue) = GoldenRatioMathod.FindMinimum(a, b, e / 10, singleVarFunc)
+        newPoint = GetSum(previousPoint, GetMultiply(direction, l))
+        trace.append((newPoint, newValue))
+
+
+def GetSum(firstVector: List[float], secondVector: List[float]) -> List[float]:    
+    if len(firstVector) != len(secondVector):
+        raise Exception("Vectors have different number of elements. First={}, second={}".format(len(firstVector), len(secondVector)))
+    result = firstVector.copy()
+    for i in range(len(firstVector)):
+        result[i] = firstVector[i] + secondVector[i]
+    return result
+
+
+def GetMultiply(vector: List[float], multiplier: float) -> List[float]:   
+    result = vector.copy()
+    for i in range(len(vector)):
+        result[i] = vector[i] * multiplier
+    return result
+
+
+def GetMatrixMultiply(matrix: List[List[float]], vector: List[float]) -> List[float]:   
+    result = vector.copy()
+    for i in range(len(vector)):
+        sum = 0
+        for j in range(len(matrix[0])):
+            sum += matrix[i][j] * vector[j]
+    return result
+
+
+def GetSignleVariableFunc(func: Callable[[List[float]], float], previousPoint: List[float], direction: List[float]) -> Callable[[float], float]:
+    def inner(l: float) -> float:
+        newPoint = GetSum(previousPoint, GetMultiply(direction, l))
+        return func(newPoint)
+    return inner
+
+
+def GetLength(vector: List[float]) -> float:
+    i = 0
+    distance = 0
+    while i < len(vector):
+        distance += vector[i] ** 2
+        i += 1
+    return math.sqrt(distance)
