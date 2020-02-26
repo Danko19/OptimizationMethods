51528ecb325f4f084825f9c3777ee0e3d2b82879
Danko <wally19@bk.ru>
2020-02-26 23:46:16 +0400

    results printing. Lab2 done

--- Lab2.py
+++ Lab2.py
@@ -16,15 +16,22 @@ def reversedMatrix(x):
     print("x={}, y={}".format(x[0], x[1]))
     return [[-1/2, 0], [0, -1/14]]
 
-func = lambda x: -(x[0] ** 2) - 7 * x[1] ** 2 + 3 * x[0] + x[1]
-result = DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.01, 0.01)
-print(result)
+results = []
+results.append((DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.01, 0.01), "DirectcoordinateDescentMethod"))
+results.append((HookeJeevesMethod.FindMinimum(function, [0, 0], 0.1, 0.01), "HookeJeevesMethod"))
+results.append((GradientDescentMethod.FindMinimum(function, gradient, [0, 0], 0.01, 0.01, 0.01), "GradientDescentMethod"))
+results.append((NewtonRaphsonMethod.FindMinimum(function, gradient, reversedMatrix, [0, 0], 0.01, 0.01, 0.01), "NewtonRaphsonMethod"))
 
-result = HookeJeevesMethod.FindMinimum(function, [0, 0], 0.1, 0.01)
-print(result)
-
-result = GradientDescentMethod.FindMinimum(function, gradient, [0, 0], 0.01, 0.01, 0.01)
-print(result)
-
-result = NewtonRaphsonMethod.FindMinimum(function, gradient, reversedMatrix, [0, 0], 0.01, 0.01, 0.01)
-print(result)
+print()
+for (result, methodName) in results:
+    print()
+    print("{} results:".format(methodName))
+    stepNumber = 0
+    for step in result:
+        print("Step {}:".format(stepNumber))
+        stepNumber += 1
+        dimNumber = 1
+        for dim in step[0]:
+            print("  x{}={}".format(dimNumber, dim))
+            dimNumber += 1
+        print("  f(x)={}".format(step[1]))
