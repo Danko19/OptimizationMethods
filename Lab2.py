aeadbc93003a32e8afc2f673b511364e861c7c27
Danko <wally19@bk.ru>
2020-02-26 23:05:38 +0400

    GradientDescentMethod

--- Lab2.py
+++ Lab2.py
@@ -1,10 +1,22 @@
 import DirectcoordinateDescentMethod
+import HookeJeevesMethod
+import GradientDescentMethod
 
 
 def function(x):
     print("x={}, y={}".format(x[0], x[1]))
     return x[0] ** 2 + 7 * x[1] ** 2 - 3 * x[0] - x[1]
 
+def gradient(x):
+    print("x={}, y={}".format(x[0], x[1]))
+    return [-2 * x[0] + 3, -14 * x[1] + 1]
+
 func = lambda x: -(x[0] ** 2) - 7 * x[1] ** 2 + 3 * x[0] + x[1]
-result = DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.1, 0.1)
+result = DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.01, 0.01)
+print(result)
+
+result = HookeJeevesMethod.FindMinimum(function, [0, 0], 0.1, 0.01)
+print(result)
+
+result = GradientDescentMethod.FindMinimum(function, gradient, [0, 0], 0.01)
 print(result)
