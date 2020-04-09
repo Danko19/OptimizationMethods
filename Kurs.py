069b22dd544333efafb7cf815a81fad23239d684
Danko <wally19@bk.ru>
2020-04-09 23:10:07 +0400

    3.2 NelderMeadMethod, but no visualization

--- Kurs.py
+++ Kurs.py
@@ -1,6 +1,7 @@
 import DirectcoordinateDescentMethod
 import OneDimensionalOptimizationsHelper
 import HookeJeevesMethod
+import NelderMeadMethod
 
 
 def function(X):
@@ -12,7 +13,9 @@ results = []
 results.append((DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.01, 0.01, OneDimensionalOptimizationsHelper.GetGoldenRatioMethodFunc(0.01)), "DirectcoordinateDescentWithGoldenrationMethod"))
 results.append((DirectcoordinateDescentMethod.FindMinimum(function, [0, 0], 0.01, 0.01, OneDimensionalOptimizationsHelper.GetPowellsMethodFunc(0.01, 0.01)), "DirectcoordinateDescentPowellsMethod"))
 
-results.append((HookeJeevesMethod.FindMinimum(function, [0, 0], 0.1, 0.01, OneDimensionalOptimizationsHelper.GetFibonacciMethodFunc(0.01)), "HookeJeevesWithFibonacciMethod"))
+NelderMeadMethod.FindMinimum(function, [0, 0], 0.1, 0.1)
+
+results.append((HookeJeevesMethod.FindMinimum(function, [0, 0], 0.01, 0.01, OneDimensionalOptimizationsHelper.GetFibonacciMethodFunc(0.01)), "HookeJeevesWithFibonacciMethod"))
 
 print()
 for (result, methodName) in results:
---
+++ NelderMeadMethod.py
@@ -0,0 +1,123 @@
+import math
+
+def FindMinimum(func, X0, el, ef, alfa = 3, beta = 0.5, gamma = 0.5, l = 10):
+    n = len(X0)
+    r1 = l * (math.sqrt(n + 1) + n - 1) / (n * math.sqrt(2))
+    r2 = l * (math.sqrt(n + 1) - 1) / (n * math.sqrt(2))
+    X = [X0]
+    for col in range(n):
+        X.append([])
+        for row in range(n):
+            if col == row:
+                res = X0[row] + r1
+            else:
+                res = X0[row] + r2
+            X[col + 1].append(res)
+    F = []
+    for x in X:
+        F.append(func(x))
+    trace = [(X.copy(), F.copy())]
+    while True:
+        F_sorted = F.copy()
+        F_sorted.sort(reverse=True)
+        f_h = F_sorted[0]
+        f_l = F_sorted[-1]
+        f_g = F_sorted[1]
+        i_h = F.index(f_h)
+        i_l = F.index(f_l)
+        i_g = F.index(f_g)
+        x_h = X[i_h]
+        x_l = X[i_l]
+        x_g = X[i_g]
+        x_n2 = get_x_n2(X, i_h)
+        x_n3 = minus(multiply(x_n2, 2), x_h)
+        f_n3 = func(x_n3)
+        if f_n3 <= f_l:
+            x_n4 = plus(multiply(x_n2, 1 - alfa), multiply(x_n3, alfa))
+            f_n4 = func(x_n4)
+            if f_n4 <= f_n3:
+                X[i_h] = x_n4
+                F[i_h] = f_n4
+            else:
+                X[i_h] = x_n3
+                F[i_h] = f_n3
+        elif f_g < f_n3 < f_h:
+            x_n5 = plus(x_n2, multiply(minus(x_h, x_n2), beta))
+            f_n5 = func(x_n5)
+            X[i_h] = x_n5
+            F[i_h] = f_n5
+        elif f_l < f_n3 <= f_g:
+            X[i_h] = x_n3
+            F[i_h] = f_n3
+        elif f_n3 >= f_h:
+            for i in range(len(X)):
+                if i == i_l:
+                    continue
+                X[i] = plus(x_l, multiply(minus(X[i], x_l), gamma))
+                F[i] = func(X[i])
+        else:
+            raise Exception("Something went wrong")
+        trace.append((X.copy(), F.copy()))
+        if getSKO(F) < ef and getMaxLength(X) < el:
+            return trace
+
+def get_x_n2(X, i_h):
+    n = len(X) - 1
+    x_n2 = [0] * n 
+    for i in range(len(X)):
+        if i != i_h:
+            for j in range(n):
+                x_n2[j] += X[i][j]
+    for i in range(n):
+        x_n2[i] /= n
+    return x_n2
+
+
+def minus(X1, X2):
+    res = [0] * len(X1)
+    for i in range(len(X1)):
+        res[i] = X1[i] - X2[i]
+    return res
+
+
+def plus(X1, X2):
+    res = [0] * len(X1)
+    for i in range(len(X1)):
+        res[i] = X1[i] + X2[i]
+    return res
+
+
+def multiply(X1, m):
+    res = [0] * len(X1)
+    for i in range(len(X1)):
+        res[i] = X1[i] * m
+    return res
+
+def getSKO(F):
+    f_avg = getAVG(F)
+    D = 0
+    for i in range(len(F)):
+        D += (F[i] - f_avg) ** 2
+    return D / len(F)
+
+def getAVG(F):    
+    f_avg = 0
+    for i in range(len(F)):
+        f_avg += F[i]
+    return f_avg / len(F)
+
+def getMaxLength(X):
+    maximum = -math.inf
+    for i in range(len(X)):
+        for j in range(i + 1, len(X)):
+            dist = getDistance(X[i], X[j])
+            if dist > maximum:
+                maximum = dist
+    return maximum
+
+def getDistance(X1, X2):
+    dist = 0
+    for i in range(len(X1)):
+        dist += (X1[i] - X2[i]) ** 2
+    return math.sqrt(dist)
+
